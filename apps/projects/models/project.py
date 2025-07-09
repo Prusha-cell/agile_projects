ed542846092e5f08b0fb8f6bec387af65fb880bc
ICH_pycharm_bash <anubis2016prusha@gmail.com>
2025-07-09 13:32:40 +0200

    created model ProjectFile

--- apps/projects/models/project.py
+++ apps/projects/models/project.py
@@ -5,9 +5,41 @@ class Project(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    files = models.ManyToManyField('ProjectFile', related_name='projects')
 
     class Meta:
         ordering = ['name']
 
     def __str__(self):
         return self.name
+
+    @property
+    def count_of_files(self):
+        return self.files.count()
+
+
+# В приложении project создайте новую модель ProjectFile:
+# ● file_name Строковое поле, максимальная длина - 120 символов)
+# ● file_path Поле загрузки файлов, таргет сохранения - папка ‘documentsʼ)
+# ● created_at Дата создания файла, автозаполняется при создании нового файла)
+# ● Отображение объектов по полу name (магический метод __str__)
+# ● Сортировка записей по дате создания в порядке убывания
+# 2. В модели Project создайте новое поле files, которое будет связывать проекты и файлы связью “Многие ко
+# Многимˮ, связующее поле - “projectˮ.
+# 3. В модели Project добавьте динамическое поле count_of_files через декоратор @property, которое будет
+# высчитывать количество файлов для проекта.
+
+
+class ProjectFile(models.Model):
+    file_name = models.CharField(max_length=120)
+    file_path = models.FileField(upload_to='documents/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.file_name
+
+
+
